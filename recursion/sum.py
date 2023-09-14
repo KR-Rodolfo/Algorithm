1c4e11973b5c6dbe8b4ca1058bcca159f8dbf6a1
KR-Rodolfo <144571697+KR-Rodolfo@users.noreply.github.com>
2023-09-14 18:11:38 +0900

    create recursive_sum

---
+++ recursion/sum.py
@@ -0,0 +1,24 @@
+# O(n)
+def sum(n):
+    s = 0
+    for i in range(1, n + 1):
+        s += i
+    return s
+
+# O(n)
+def recursive_sum_1(n):
+    if n == 1:
+        return 1
+    return recursive_sum_1(n - 1) + n
+
+#O(n)
+def recursive_sum_2(a, b):
+    if a == b:
+        return a
+    m = (a + b) // 2
+    return recursive_sum_2(a, m) + recursive_sum_2(m + 1, b)
+
+n = 5
+print(sum(n))
+print(recursive_sum_1(n))
+print(recursive_sum_2(1, n))
